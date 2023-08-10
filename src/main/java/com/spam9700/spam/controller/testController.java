ecbb25bd615665ea9a58bda7970ba64647d713c7
kyb0 <byyb2696@gmail.com>
2023-08-10 14:22:54 +0900

    2

---
+++ src/main/java/com/spam9700/spam/controller/testController.java
@@ -0,0 +1,18 @@
+package com.spam9700.spam.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class testController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "test";
+    }
+ 
+    @GetMapping("/")
+    public String hi(){
+        return "hi";
+    }
+}
--- src/main/webapp/WEB-INF/views/hi.jsp
+++ src/main/webapp/WEB-INF/views/hi.jsp
@@ -6,6 +6,6 @@
     <title>Document</title>
 </head>
 <body>
-    hello! 하하하하하하
+    gqwgqwgfqwg
 </body>
 </html>
