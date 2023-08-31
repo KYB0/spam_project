7ba127986aedd3c96caf5a29fdd7b8f6fac24b11
Downychogonongchuk <renounce774@naver.com>
2023-08-31 15:53:44 +0900

    happyyunghwa

--- src/main/resources/static/js/bot.js
+++ src/main/resources/static/js/bot.js
@@ -16,108 +16,82 @@ function sendMessage(message) {
     const modal = document.getElementById("chatbotModal");
     const modalContent = modal.querySelector('.modal-content');
 
-    if (message === '회원가입') {
-        const responseMessage = document.createElement('div');
-        responseMessage.className = 'chat-message bot';
-        responseMessage.textContent = '회원가입을 원하시면 아래 링크를 클릭해주세요.';
-        
-        const link = document.createElement('a');
-        link.href = "/spam/member/joinfrm" // 여기에 실제 링크 주소를 넣어주세요
-        link.textContent = '회원가입 페이지로 이동하기';
-        responseMessage.appendChild(document.createElement('br'));
-        responseMessage.appendChild(link);
-
-        modalContent.appendChild(responseMessage);
-
-    } else if (message === '로그인') {
-        const responseMessage = document.createElement('div');
-        responseMessage.className = 'chat-message bot';
-        responseMessage.textContent = '로그인을 원하시면 아래 링크를 클릭해주세요';
-        // 로그인 창 관련 메시지와 링크 추가 처리
-
-        const link = document.createElement('a');
-        link.href = "/spam/member/i_login"
-        link.textContent = '로그인 페이지로 이동하기';
-        responseMessage.appendChild(document.createElement('br'));
-        responseMessage.appendChild(link);
-
-        modalContent.appendChild(responseMessage);
-
-    } else if (message === '아이디찾기') {
-        const responseMessage = document.createElement('div');
-        responseMessage.className = 'chat-message bot';
-        responseMessage.textContent = '아이디 찾기를 원하시면 아래 링크를 클릭해주세요';
-        // 아이디 찾기 관련 메시지와 링크 추가 처리
-
-        const link = document.createElement('a');
-        link.href =
-        link.textContent = '아이디 찾기 페이지로 이동하기';
-        responseMessage.appendChild(document.createElement('br'));
-        responseMessage.appendChild(link);
-
-        modalContent.appendChild(responseMessage);
-
-
-    } else if (message === '리뷰는 어디에 남기나요?') {
-        const responseMessage = document.createElement('div');
-        responseMessage.className = 'chat-message bot';
-        responseMessage.textContent = '리뷰 남기기를 원하시면 아래 링크를 클릭해주세요';
-        
-        const link = document.createElement('a');
-        link.href =
-        link.textContent = '리뷰 페이지로 이동하기';
-        responseMessage.appendChild(document.createElement('br'));
-        responseMessage.appendChild(link);
-
-        modalContent.appendChild(responseMessage);
-        
-
-    } else if (message === '리뷰는 어디에 남기나요?') {
-        const responseMessage = document.createElement('div');
-        responseMessage.className = 'chat-message bot';
-        responseMessage.textContent = '리뷰 남기기를 원하시면 아래 링크를 클릭해주세요';
-        
-        const link = document.createElement('a');
-        link.href =
-        link.textContent = '리뷰 페이지로 이동하기';
-        responseMessage.appendChild(document.createElement('br'));
-        responseMessage.appendChild(link);
-
-        modalContent.appendChild(responseMessage);
-
-
-
+    if (message === '개인정보') {
+        showOptions(['가입/탈퇴', '로그인', '정보변경', '민원처리신청']);
+    } else if (message === '독서실') {
+        showOptions(['예약상품 결제', '단체예약/장소대관', '비품대여']);
+    } else if (message === '사업자 등록') {
+        showOptions(['등록/입점', '퇴점신청', '탈퇴', '가격/정보변경']);
+    } else if (message === '기타상담') {
+        showOptions(['칭찬/불만사항 작성', '스팸/광고/제휴 문의']);
     } else {
         // 기타 메시지 처리
     }
 
-    closeChatbot(); 
-
+    closeChatbot();
 }
 
 function showOptions(options) {
     const modalContent = document.querySelector('.modal-content');
     modalContent.innerHTML = ''; // 기존 내용 초기화
-        
+
     for (const option of options) {
         const optionButton = document.createElement('button');
         optionButton.className = 'chat-button';
         optionButton.textContent = option;
-        optionButton.onclick = () => sendMessage(option);
+        optionButton.onclick = () => showSubOptions(option);
         modalContent.appendChild(optionButton);
     }
 }
 
-// function sendMessage(message) {
-//     const modal = document.getElementById("chatbotModal");
-//     // ... 메시지 전송 및 응답 처리 등의 로직
-//     // 백엔드와 통신하여 응답을 받아온다고 가정하면, 응답 내용을 모달 안에 추가할 수 있습니다.
-//     const responseMessage = document.createElement('div');
-//     responseMessage.className = 'chat-message bot';
-//     responseMessage.textContent = message;
+function showSubOptions(parentOption) {
+    const modalContent = document.querySelector('.modal-content');
+    modalContent.innerHTML = ''; // 기존 내용 초기화
 
-//     const modalContent = modal.querySelector('.modal-content');
-//     modalContent.appendChild(responseMessage);
+    const subOptions = [];
+    // 여기에 해당 parentOption 에 따른 하위 옵션을 추가 예정
+    if (parentOption === '가입/탈퇴') {
+        subOptions.push('회원 탈퇴는 어떻게 하나요?', '미성년자는 가입 못 하나요?', '회원가입 시 휴대폰 본인인증 오류가 발생됩니다');
+    } else if (parentOption === '로그인') {
+        subOptions.push('로그인 시 등록되지 않은 아이디라고 나옵니다.', '로그인이 안되는 경우는 어떻게 하나요?', '로그인 시 휴먼계정 안내가 됩니다');
+    }
+    // 나머지 parentOption 에 따른 하위 옵션 추가 예정
+    showOptions(subOptions);
 
-//     closeChatbot(); // 선택 후 모달 닫기
-// }
+
+    modalContent.innerHTML = ''; // 기존 내용 초기화
+    // 하위 옵션 버튼 생성 및 클릭 이벤트 할당
+    for (const optionText of subOptions) {
+        const optionButton = document.createElement('button');
+        optionButton.className = 'chat-button';
+        optionButton.textContent = optionText;
+        optionButton.onclick = function() {
+            handleOptionClick(optionText);
+        };
+        modalContent.appendChild(optionButton);
+    }
+}
+
+// 1안 새로운 버튼 처리 코드 추가 
+function handleOptionClick(option) {
+    if (option === '회원 탈퇴는 어떻게 하나요?') {
+        console.log(11111);
+        const modalContent = document.querySelector('.modal-content');
+        modalContent.innerHTML = ''; // 기존 내용 초기화
+
+        const responseMessage = document.createElement('div');
+        responseMessage.className = 'chat-message bot';
+        responseMessage.textContent = '회원 탈퇴를 원하시면 아래 링크를 클릭해주세요.';
+
+        const link = document.createElement('a');
+        link.href = "/spam/member/withdrawal"; // 회원 탈퇴 페이지 링크
+        link.textContent = '회원 탈퇴 페이지로 이동하기';
+        responseMessage.appendChild(document.createElement('br'));
+        responseMessage.appendChild(link);
+
+        modalContent.appendChild(responseMessage);
+    }
+}
+
+
+
--- src/main/webapp/WEB-INF/views/bot.jsp
+++ src/main/webapp/WEB-INF/views/bot.jsp
@@ -1,5 +1,6 @@
 ﻿<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,30 +10,28 @@
     <script src="/spam/js/bot.js"></script>
     <link rel="stylesheet" href="/spam/css/bot.css">
 </head>
+
 <body>
     <%@ include file="header.jsp" %>
     <button type="button" style="border:0" class="btn_bot" onclick="showChatbot()">
         <img src="/spam/img/bot.png" alt="챗봇 아이콘" class="chatbot-icon">
     </button>
-    
-  <!-- 모달 창 -->
-  <div id="chatbotModal" class="modal" onclick="closeModal()">
-    <div class="modal-content" onclick="stopPropagation(event)">
-        <span class="close" onclick="closeChatbot()">&times;</span>
-        
-        <button class="chat-button" onclick="showOptions(['회원가입', '로그인', '아이디찾기'])">로그인 관련</button>
-        <button class="chat-button" onclick="showOptions(['리뷰는 어디에 남기나요?', '리뷰페이지', '내가 작성한 리뷰 페이지를 보고싶어요'])">리뷰 관련</button>
-        <button class="chat-button" onclick="showOptions(['비품대여 방법을 알고싶어요', '내가 대여한 비품', '대여가능한 비품 종류'])">비품 관련</button>
-        
+
+    <!-- 모달 창 -->
+    <div id="chatbotModal" class="modal" onclick="closeModal()">
+        <div class="modal-content" onclick="stopPropagation(event)">
+            <span class="close" onclick="closeChatbot()">&times;</span>
+
+            <button class="chat-button" onclick="showOptions(['가입/탈퇴', '로그인', '정보변경', '민원처리신청'])">개인정보</button>
+            <button class="chat-button" onclick="showOptions(['예약상품 결제', '단체예약/장소대관', '비품대여'])">독서실</button>
+            <button class="chat-button" onclick="showOptions(['등록/입점', '퇴점신청', '탈퇴', '가격/정보변경'])">사업자 등록</button>
+            <button class="chat-button" onclick="showOptions(['칭찬/불만사항 작성', '스팸/광고/제휴 문의'])">기타상담</button>
+
+        </div>
+
     </div>
 
-</div>
-    
-     
-    </body>
-    </html> 
 
-
-    <%@ include file="footer.jsp" %>
 </body>
 </html>
+<%@ include file="footer.jsp" %>
