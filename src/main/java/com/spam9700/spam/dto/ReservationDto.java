f6d19dcafc215b36997c48aa80af431a0b2cf9b3
kyb0 <byyb2696@gmail.com>
2023-09-16 18:53:57 +0900

    123

---
+++ src/main/java/com/spam9700/spam/dto/ReservationDto.java
@@ -0,0 +1,21 @@
+package com.spam9700.spam.dto;
+
+import java.time.LocalDateTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class ReservationDto {
+    private int reservation_id;
+    private String customer_id;
+    private String seat_number;
+    private int room_id;
+    private String status;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime start_time;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime end_time;
+
+}
