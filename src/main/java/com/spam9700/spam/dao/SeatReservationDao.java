5a7531144efedca3ecdf998ff4b68b3a052d0433
haeun <che9663@naver.com>
2023-09-19 04:48:42 +0900

    0919 차쌤한테 물어보면 될듯

--- src/main/java/com/spam9700/spam/dao/SeatReservationDao.java
+++ src/main/java/com/spam9700/spam/dao/SeatReservationDao.java
@@ -1,4 +1,6 @@
 package com.spam9700.spam.dao;
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.data.repository.query.Param;
 
@@ -15,4 +17,18 @@ public interface SeatReservationDao {
     String getRoomNameByRoomId(String room_id);
 
     String getCustomerById(@Param("customer_id") String customerId);
+
+    void insertReservations(ReservationDto reservationDto);
+
+     // 예약 조회 메서드 (고객 ID로 조회)
+     List<ReservationDto> selectReservationsByCustomerId(@Param("customer_id") String customer_id);
+
+    boolean checkCustomerExistence(String customer_id);
+
+    boolean checkSeatExistence(String seat_number);
+
+    boolean checkRoomExistence(int room_id);
+
+    void savePaymentRecord(ReservationDto rsd);
+
 }
