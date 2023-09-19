5ec729f5f477a76c243ba9b6ce4a75b1ecbc7b05
haeun112 <che9663@naver.com>
2023-09-19 17:51:36 +0900

    0919 ㄹㅇ 마지막

--- src/main/java/com/spam9700/spam/controller/ReservationController.java
+++ src/main/java/com/spam9700/spam/controller/ReservationController.java
@@ -1,4 +1,5 @@
 ﻿package com.spam9700.spam.controller;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -20,39 +21,49 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import com.spam9700.spam.dto.ReservationDto;
 import com.spam9700.spam.dto.SeatDto;
 import com.spam9700.spam.dto.ReservationDto;
 import com.spam9700.spam.service.MemberService;
 import com.spam9700.spam.service.SeatReservationService;
 import com.spam9700.spam.service.StudycafeService;
+
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 @Controller
 @RequestMapping("/{room_name}")
 public class ReservationController {
+
     @Autowired
     private StudycafeService studycafeService;
+
     @Autowired
     private MemberService memberService;
+
     @Autowired
     private SeatReservationService seatReservationService;
+
     // 기존의 상세 페이지 컨트롤러와 겹치지 않도록 별도의 URL을 사용
     @GetMapping("/reservation")
     public String reservationPage(HttpSession session, @PathVariable("room_name") String room_name, Model model) {
+
         // studycafeService를 이용하여 room_name을 기반으로 좌석 정보를 가져오는 service 메서드 호출
         List<SeatDto> seats = studycafeService.getSeatsByRoomName(room_name);
         // 모델에 좌석 정보를 추가하여 jsp 페이지로 전달
         // for(SeatDto sd : seats){
         // System.out.println(sd);
         // }
+
         List<Integer> seatNumbersList = new ArrayList<>();
         String seatNumber = null;
         String[] seatNumbers = null;
         for (SeatDto seat : seats) {
             seatNumber = seat.getSeat_number();
             seatNumbers = seatNumber.split(",");
+
             for (String number : seatNumbers) {
                 try {
                     int parsedNumber = Integer.parseInt(number.trim());
@@ -64,12 +75,15 @@ public class ReservationController {
                 }
             }
         }
+
         model.addAttribute("seats", seats);
         model.addAttribute("room_name", room_name);
         // model.addAttribute("seatNumber", seatNumber);
         model.addAttribute("seatNumbersList", seatNumbersList);
+
         return "seat"; // 예약 페이지 템플릿 이름 또는 경로
     }
+
     // 방 ID를 세션에서 가져오는 메서드
     private int getRoomIdFromSession(HttpSession session) {
         // 세션에서 방 ID를 가져오는 로직을 구현해야 합니다.
@@ -81,6 +95,7 @@ public class ReservationController {
         // 세션에서 "room_id" 속성을 찾을 수 없는 경우 예외 처리 또는 기본값 반환
         return 1; // 기본값으로 1을 반환합니다. 수정이 필요한 경우 예외 처리 등을 수행하세요.
     }
+
     // 고객 ID를 세션에서 가져오는 메서드
     private String getCustomerIdFromSession(HttpSession session) {
         // 세션에서 고객 ID를 가져오는 로직을 구현해야 합니다.
@@ -94,18 +109,17 @@ public class ReservationController {
     }
 
 
-
     // 결제 완료 처리
-            @PostMapping("/payment")
-            public String processPayment(@PathVariable String room_name,
+    @PostMapping("/payment")
+    public String processPayment(@PathVariable String room_name,
             @RequestParam("room_id") int room_id,
             @RequestParam("seat_number") String seat_number,
             @RequestParam("start_time") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm") LocalDateTime startTime,
             @RequestParam("end_time") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm") LocalDateTime endTime,
             HttpSession session) { // HttpSession을 파라미터로 받아 세션을 사용합니다.
-
+    
         String customer_id = getCustomerIdFromSession(session); // 세션에서 고객 ID를 가져옵니다.
-
+    
         ReservationDto reservationDto = new ReservationDto();
         reservationDto.setCustomer_id(customer_id); // 세션에서 가져온 고객 ID를 설정합니다.
         reservationDto.setSeat_number(seat_number);
@@ -113,15 +127,15 @@ public class ReservationController {
         reservationDto.setStatus("1"); // 결제가 완료되었음을 나타내는 상태
         reservationDto.setStart_time(startTime);
         reservationDto.setEnd_time(endTime);
-
     
         // 데이터베이스에 저장
         seatReservationService.saveReservation(reservationDto);
-
-
+    
         return "redirect:/main"; // 결제가 성공적으로 완료되면 메인 페이지로 리다이렉트
     }
 // 이렇게 하면 콤마로 구분된 seat_number 목록에 대한 예약을 처리할 수 있으며, LocalDateTime 형식을 사용하여 "T"를 공백으로 대체하지 않습니다.
+
+
 
 
 
@@ -131,6 +145,7 @@ public class ReservationController {
 //     public String saveReservations(@RequestBody ReservationDto reservationDto) {
 //         // 서비스 클래스를 호출하여 예약 데이터 저장
 //         seatReservationService.saveReservations(reservationDto);
+
 //         return "redirect:/main"; // 저장 성공 후 리다이렉트할 페이지
 //     }
 
@@ -146,6 +161,7 @@ public String getReservationPage(HttpSession session, @PathVariable("room_name")
    // start_timeStr와 end_timeStr에서 "T"를 공백으로 대체
    String start_timeStr = start_time.toString().replace("T", " ");
    String end_timeStr = end_time.toString().replace("T", " ");
+
    log.info("Received start_time: {}", start_timeStr);
    log.info("Received end_time: {}", end_timeStr);
 
@@ -156,10 +172,10 @@ public String getReservationPage(HttpSession session, @PathVariable("room_name")
    model.addAttribute("start_time", start_timeStr);
    model.addAttribute("end_time", end_timeStr);
    model.addAttribute("customer_id", customer_id);
+
    return "payment"; // 예약 페이지 템플릿 이름 또는 경로
 }
 
 
-    
 
 }       
